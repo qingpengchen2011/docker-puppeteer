b5be3f263cb2fdca6d840b71a638ca7719b6d03a
cqp <cqp@yihui.com>
2021-01-26 08:57:43 +0800

    add test script

---
+++ index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer')
+ 
+async function printPDF() {
+	  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+	  const page = await browser.newPage();
+	  await page.goto('https://www.baidu.com/', {waitUntil: 'networkidle0'});
+	  const pdf = await page.pdf({ format: 'A4',path: 'a.pdf' });
+	  console.log("write a.pdf"); 
+	  await browser.close();
+	  return pdf
+};
+
+
+(async () => {
+	    await printPDF();
+})();
+
+
